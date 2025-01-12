1481092dda915089c87f8b5ecad29fae2fcf2ca5
akshyv <akshy.nattu@gmail.com>
2025-01-12 15:45:27 +0530

    try

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes.todos import todos_bp
 from utils.db_helper import init_db
 import os
 import sys
-#
+
 
 # Add the server directory to Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
